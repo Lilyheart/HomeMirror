da083e28e0e966d9c7841829041d361ec2c36245
Lily <lily.programs@gmail.com>
2016-04-01 15:50:53 -0400

    Add Test Class

---
+++ app/src/main/java/com/morristaedt/mirror/modules/Test.java
@@ -0,0 +1,11 @@
+package com.morristaedt.mirror.modules;
+
+/**
+ * Created by Lilyheart on 4/1/2016.
+ *
+ */
+public class Test {
+    //This is a test class for learning how to use VC on AS
+    int test = 1;
+
+}
