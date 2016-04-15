58eb1ee3b6aba84e432362692c4eff3eca5c0194
Lily <lily.programs@gmail.com>
2016-04-15 14:27:50 -0400

    Progress on screens.  Text and clock vertical.

---
+++ app/src/main/java/com/morristaedt/mirror/MonitorScreen.java
@@ -0,0 +1,16 @@
+package com.morristaedt.mirror;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.morristaedt.mirror.R;
+
+public class MonitorScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monitor_screen);
+    }
+
+}
