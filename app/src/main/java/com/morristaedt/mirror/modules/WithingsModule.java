2615c31e16e107e5b5ae783698a52d0ea47bad03
chelms7125 <caitlyn.helms@yahoo.com>
2016-05-09 19:26:06 -0400

    Add comments to withings code

--- app/src/main/java/com/morristaedt/mirror/modules/WithingsModule.java
+++ app/src/main/java/com/morristaedt/mirror/modules/WithingsModule.java
@@ -52,6 +52,8 @@ public class WithingsModule {
             protected String doInBackground(Void... params) {
                 try {
 
+                    //Declare new variables to hold possible measurements.
+                    //Initialized to -99 which will indicate if they get new values
                     double weight = -99;
                     double height = -99;
                     double leanMass = -99;
@@ -61,9 +63,8 @@ public class WithingsModule {
                     double systolic = -99;
                     double pulse = -99;
                     double sp02Percent = -99;
+
                     int mostRecentWeighInEpoch;
-                    Date mostRecentWeighInDate = new Date(0);
-                    Date currMeasureGroupDate;
                     int hoursAgo = 0;
                     int daysAgo = 0;
 
@@ -71,8 +72,10 @@ public class WithingsModule {
                     Random randNum = new Random();
                     oauth_nonce = randNum.nextInt(100000);
 
+                    // Convert current time into UNIX epoch
                     long OAUTHTimeStamp = System.currentTimeMillis() / 1000L;
 
+                    // Build URL to access the Withings health information
                     String wURL = "https://wbsapi.withings.net/measure?action=getmeas&userid="+withings_userid+
                             "&lastupdate=1459468800&oauth_consumer_key="+withings_con_key+
                             "&oauth_nonce="+oauth_nonce+"&oauth_signature="+withings_sig+
@@ -88,53 +91,53 @@ public class WithingsModule {
                     HttpURLConnection request = (HttpURLConnection)url.openConnection();
                     request.connect();
 
-                    // Parse the input stream into a json element using Gson library
+                    // Create new JsonParson object
                     JsonParser jp = new JsonParser();
 
-                    // TODO Caitlyn comment
+                    // Converts the input stream to a json element
                     JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
 
-                    // TODO Caitlyn comment
+                    // converts the json element to a json object
                     JsonObject rootObj = root.getAsJsonObject();
 
-                    // TODO Caitlyn comment
+                    // Takes the JsonObject and get the json item with the body key,
+                    // convert it an object and then get the json item with the measuregrps key as an Array
                     JsonArray measureGroups = rootObj.get("body").getAsJsonObject().get("measuregrps").getAsJsonArray();
 
-                    // TODO Caitlyn comment
+                    // The For loop steps through the measureGroups JsonArray
                     for (int j = 0; j < measureGroups.size(); j++)
                     {
 
-                        // TODO Caitlyn comment
+                        // Gets the index item of the array - adding the .getAsJsonObject() we save it directly into another JsonObject
                         JsonObject nextMeasureGroup =  measureGroups.get(j).getAsJsonObject();
 
-                        // TODO Caitlyn comment
+                        // Gathers the time stamp from last entry as an Int
                         mostRecentWeighInEpoch = nextMeasureGroup.get("date").getAsInt();
-                        currMeasureGroupDate = new Date((nextMeasureGroup.get("date").getAsLong() * 1000L));
 
-                        // TODO Caitlyn comment
+                        // gets the json item with the measures key as an array
                         JsonArray measures =  nextMeasureGroup.get("measures").getAsJsonArray();
 
-                        // TODO Caitlyn comment
+                        // The inner for loop goes through the measures array to obtain the measures values
                         for (int i = 0; i < measures.size(); i++)
                         {
-                            // TODO Caitlyn comment
+                            // Again gets the index item of the Array and saves it directly as an object
                             JsonObject next = measures.get(i).getAsJsonObject();
-                            // TODO Caitlyn comment
+                            // Grabs the "Value" and uses Math.pow to translates it to it's appropriate SI value.
                             double value = next.get("value").getAsLong() * Math.pow(10, next.get("unit").getAsLong());
 
-                            // TODO Caitlyn comment
+                            // This gathers the type of measure and stores it as an Int
+                            // Withings uses and integer key to indicate which measurement is being utilized.
                             switch(next.get("type").getAsInt())
                             {
-                                case  1: //Weight (kg)
+                                case  1: //Weight (kg) - multiply by 2.2046 to get lbs.
                                     if (weight == -99) {
                                         weight = (value * 2.2046);
-                                        mostRecentWeighInDate = currMeasureGroupDate;
                                         //Calculate Easy to read how long ago hoursAgo daysAgo
                                         if ((OAUTHTimeStamp - mostRecentWeighInEpoch) <= 1*24*60*60) // Less than 1 day in seconds
                                         {
                                             hoursAgo = (int)(OAUTHTimeStamp - mostRecentWeighInEpoch) / 60 / 60;
                                         }
-                                        else
+                                        else // More then a day
                                         {
                                             daysAgo = (int)(OAUTHTimeStamp - mostRecentWeighInEpoch) / 60 / 60 / 24;
                                         }
@@ -143,13 +146,13 @@ public class WithingsModule {
                                 case  4: //Height (meter)
                                     if (height == -99) height = value;
                                     break;
-                                case  5: //Fat Free Mass (kg)
+                                case  5: //Fat Free Mass (kg) - multiply by 2.2046 to get lbs.
                                     if (leanMass == -99) leanMass = (value * 2.2046);
                                     break;
                                 case  6: //Fat Ratio (%)
                                     if (fatRatio == -99) fatRatio = value;
                                     break;
-                                case  8: //Fat Mass Weight (kg)
+                                case  8: //Fat Mass Weight (kg) - multiply by 2.2046 to get lbs.
                                     if (fatMass == -99) fatMass = (value * 2.2046);
                                     break;
                                 case  9: //Diastolic Blood Pressure (mmHg)
@@ -170,15 +173,15 @@ public class WithingsModule {
                         }
                     }
 
-                    // TODO Caitlyn comment
+                    // Once the information is received and stored it is displayed for the user to see
                     withingsDisplayInfo = String.format("You weigh: %.1f lbs\n" + "Your body fat percentage: %.1f%%\n" +
                             "Your last weight in was: \n" +
                             ((hoursAgo > 0) ? hoursAgo + " hour"+((hoursAgo > 1) ? "s" : "" )+" ago." :  daysAgo + " day"+((daysAgo > 1) ? "s" : "" )+" ago."), weight, fatRatio);
 
-
+                    // returns the string above
                     return withingsDisplayInfo;
                 } catch (Exception err) {
-                    Log.d(TAG, "getWDI thrown: " + err);
+                    Log.d(TAG, "getWithingsDI thrown: " + err);
                     return null;
                 }
             }
